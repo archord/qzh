910a46f3adcb5e30b8bf2ae15729ba8e5d620308
prepat <510948205@qq.com>
2015-03-15 17:12:56 +0800

    xxx

--- src/main/webapp/core/coreApp/map/controller/MapController.js
+++ src/main/webapp/core/coreApp/map/controller/MapController.js
@@ -1,208 +1,101 @@
-Ext.require([
-    'GeoExt.data.MapfishPrintProvider',
-    'GeoExt.panel.PrintMap'
-]);
-
-
-OpenLayers.ProxyHost = "./proxy?targetURL=";
-
-var layerArr;
-var curShp;
-var retlayer;
-var mapPanel;
-
-var id2map = [
-    {"id": 350, "base": "A", "shp": "a0", tag: "tuocun0"},
-    {"id": 10, "base": "A", "shp": "a1", tag: "tuocun1"},
-    {"id": 11, "base": "A", "shp": "a2", tag: "tuocun2"},
-    {"id": 12, "base": "A", "shp": "a3", tag: "tuocun3"},
-    {"id": 13, "base": "A", "shp": "a4", tag: "tuocun4"},
-    {"id": 14, "base": "A", "shp": "a5", tag: "tuocun5"},
-    {"id": 303, "base": "B", "shp": "b0", tag: "youcun0"},
-    {"id": 18, "base": "B", "shp": "b1", tag: "youcun1"},
-    {"id": 19, "base": "B", "shp": "b2", tag: "youcun2"},
-    {"id": 20, "base": "B", "shp": "b3", tag: "youcun3"},
-    {"id": 21, "base": "B", "shp": "b4", tag: "youcun4"},
-    {"id": 22, "base": "B", "shp": "b5", tag: "youcun5"},
-    {"id": 23, "base": "B", "shp": "b6", tag: "youcun6"},
-    {"id": 24, "base": "B", "shp": "b7", tag: "youcun7"}
-];
-
-
-function findLayerByName(name)
-{
-    for (var i = 0; i < layerArr.length; i++)
-    {
-        if (name == layerArr[i].name)
-        {
-            return layerArr[i];//retlayer; 
-            break;
-        }
-    }
-}
-
-
-var hilightLayer;
-
-var txtCBFMC = Ext.create('Ext.form.field.Text', {
-    xtype: 'textfield',
-    fieldLabel: '所有权人姓名',
-    width: 250,
-    name: 'CBFMC'
-});
-
-var btnSearch = Ext.create('Ext.button.Button', {
-    text: 'go',
-    bodyPadding: 10,
-    enableToggle: true,
-    handler: function () {
-
-         mapPanel = Ext.getCmp("mapPanelId");
-//        if (hilightLayer)
-//        {
-//            mapPanel.map.removeLayer(hilightLayer);
-//        }
-        hilightLayer = new OpenLayers.Layer.Vector(curShp, {
-            protocol: new OpenLayers.Protocol.WFS({
-                version: "1.0.0",
-                url: "http://localhost:8081/geoserver/cite/wfs",
-                featureType: curShp,
-                featureNS: "http://www.opengeospatial.net/cite"
-            }),
-            filter: new OpenLayers.Filter.Comparison({//比较操作符
-                type: OpenLayers.Filter.Comparison.LIKE, //精确查询
-                property: "cite:CBFMC",
-                value: "*" + txtCBFMC.value + "*"
-            }),
-            strategies: [new OpenLayers.Strategy.Fixed()]
-        });
-        //alert(curShp);
-        mapPanel.map.addLayer(hilightLayer);
-    }
-});
-
-var printProvider = Ext.create('GeoExt.data.MapfishPrintProvider', {
-    id: "printProvider",
-    method: "GET", //"POST", //recommended for production use
-    capabilities: printCapabilities, // provide url instead for lazy loading
-    customParams: {
-        mapTitle: "当前视图",
-        comment: ""
-    }
-});
-
-//alert(printProvider);
-//var printExtent = Ext.create('GeoExt.plugins.PrintExtent', {
-//    printProvider: Ext.create('GeoExt.data.MapfishPrintProvider', {
-//    id: "printProvider",
-//    method: "GET", //"POST", //recommended for production use
-//    capabilities: printCapabilities, // provide url instead for lazy loading
-//    customParams: {
-//        mapTitle: "当前视图",
-//        comment: ""
-//    }
-//})
-//});
-
-
-var btnScreenshot = Ext.create('Ext.button.Button', {
-    text: '保存截图',
-    bodyPadding: 10,
-    enableToggle: true,
-    handler: function () {
-        var printPage = Ext.create('GeoExt.data.PrintPage', {
-            printProvider: printProvider
-        });
-        printPage.fit(mapPanel, false);
-        printProvider.print(mapPanel, printPage);
-
-//        mapPanel.plugins[0].print();
-    }
-});
-
-
-
-
-
 Ext.define("core.map.controller.MapController", {
-    extend: "Ext.app.Controller",
-    init: function () {
-        var filepath = '/data/layers.json';
-        loadMapParameter(filepath);
-        mapPanel = Ext.getCmp("mapPanelId");
-        searchPanel = Ext.getCmp("searchPanel");
-        searchPanel.items.add(txtCBFMC);
-        searchPanel.items.add(btnSearch);
-        searchPanel.items.add(btnScreenshot);
-        
-//        mapPanel.plugins[0].addPage();
-
-        var self = this;
-        this.control({
-            "mapLayout": {
-                beforeshow: function (layout, opt) {
-                }
-            },
-            "orgTreeMap": {
-                itemclick: function (tree, record, item, index, e, eOpts) {
-//                    console.log(record.get('id'));
-//                    console.log(record.get('orgName'));
-//                    console.log(item);
-
-                    while (mapPanel.map.layers.length > 0) {
-                        mapPanel.map.removeLayer(mapPanel.map.layers[0]);
-                    }
-                    for (var i = 0; i < id2map.length; i++) {
-                        if (record.get('id') == id2map[i].id) {
-                            mapPanel.map.addLayer(findLayerByName(id2map[i].base));
-                            mapPanel.map.addLayer(findLayerByName(id2map[i].shp));
-                            curShp = id2map[i].tag;
-                        }
-                    }
-                    
-                    
-                }
-            }
-        });
-
-        function loadMapParameter(nodeId) {
-            var filepath = '/data/layers.json';
-            OpenLayers.Request.GET({
-                url: "jsonServlet?filepath=" + filepath,
-                params: {
-                    num: 100
-                },
-                success: function (response) {
-                    layerArr = new Array();
-                    var format = new OpenLayers.Format.JSON();
-                    var layers = format.read(response.responseText);
-                    for (var i = 0; i < layers.length; i++) {
-                        layerArr[i] = new OpenLayers.Layer.WMS(layers[i].name, layers[i].url,
-                                {
-                                    layers: layers[i].params.layers,
-                                    transparent: layers[i].params.transparent
-                                }, {
-                            isBaseLayer: layers[i].options.isBaseLayer,
-                            buffer: layers[i].options.buffer,
-                            visibility: layers[i].options.visibility,
-                            projection: layers[i].options.projection,
-                            maxExtent: new OpenLayers.Bounds(
-                                    layers[i].options.maxExtent.left, layers[i].options.maxExtent.bottom,
-                                    layers[i].options.maxExtent.right, layers[i].options.maxExtent.top
-                                    )
-                        });
-                    }
-                },
-                failure: function (response) {
-                    alert("Sorry, there was an error requesting data !!!");
-                }
-            });
+//	mixins:{
+//		gridUtils:"core.map.utils.GridUtils"
+//	},
+//	
+  extend: "Ext.app.Controller",
+  init: function() {
+    var self = this;
+    this.control({
+      /**showbefor*/
+      "mapLayout": {
+        beforeshow: function(layout, opt) {
+          //alert(layout.getXType());
+//          var grid = layout.down("product_grid");
+//          var store = grid.getStore();
+//          store.filter("id", 0);
         }
-    },
-    views: [
-        "core.map.view.MapLayout",
-        "core.map.view.OrgTreeMap"
-    ],
-    stores: ["core.map.store.OrgStore"],
-    models: []
+      },
+      "orgTreeMap": {
+        itemclick: function(tree, record, item, index, e, eOpts) {
+          var nodeId = record.raw.orgId;
+          //console.log(nodeId);
+          loadMapParameter(nodeId);
+        }
+      }
+
+    });
+
+    function loadMapParameter(nodeId) {
+
+      var filepath = 'data/layers.json';
+      OpenLayers.Request.GET({
+        url: "jsonServlet?filepath=" + filepath,
+        params: {
+          num: 100
+        },
+        success: function(response) {
+          var layerArr = new Array();
+          var format = new OpenLayers.Format.JSON();
+          var layers = format.read(response.responseText);
+          for (var i = 0; i < layers.length; i++) {
+            layerArr[i] = new OpenLayers.Layer.WMS(layers[i].name, layers[i].url,
+                    {
+                      layers: layers[i].params.layers,
+                      transparent: layers[i].params.transparent
+                    }, {
+              isBaseLayer: layers[i].options.isBaseLayer,
+              buffer: layers[i].options.buffer,
+              visibility: layers[i].options.visibility,
+              projection: layers[i].options.projection,
+              maxExtent: new OpenLayers.Bounds(
+                      layers[i].options.maxExtent.left, layers[i].options.maxExtent.bottom,
+                      layers[i].options.maxExtent.right, layers[i].options.maxExtent.top
+                      )
+            });
+          }
+
+          var mapPanel = Ext.getCmp("mapPanelId");
+          //console.log(mapPanel);
+          mapPanel.map.addLayer(layerArr[0]);
+          mapPanel.map.addLayer(layerArr[1]);
+
+          // 弹出层显示点击的区域元素的属性
+          var featureInfo = new OpenLayers.Control.WMSGetFeatureInfo({
+            url: '/geoserver/cite/wms',
+            title: 'Identify features by clicking',
+            //infoFormat: 'application/gml+xml',
+            //format: new OpenLayers.Format.GML,
+            queryVisible: true,
+            output: 'features',
+            eventListeners: {
+              "getfeatureinfo": function(event) {
+                var feature = event;
+                mapPanel.map.addPopup(new OpenLayers.Popup.FramedCloud(
+                        "chicken",
+                        mapPanel.map.getLonLatFromPixel(event.xy),
+                        null,
+                        event.text,
+                        null,
+                        true
+                        ));
+              }
+            }
+          });
+          mapPanel.map.addControl(featureInfo);
+          featureInfo.activate();
+          // =============== notify(): ============== 
+        },
+        failure: function(response) {
+          alert("Sorry, there was an error requesting data !!!");
+        }
+      });
+    }
+  },
+  views: [
+    "core.map.view.MapLayout",
+    "core.map.view.OrgTreeMap"
+  ],
+  stores: ["core.map.store.OrgStore"],
+  models: []
 });
--- src/main/webapp/core/coreApp/map/view/MapLayout.js
+++ src/main/webapp/core/coreApp/map/view/MapLayout.js
@@ -1,84 +1,47 @@
 Ext.require([
-    'Ext.container.Viewport',
-    'Ext.layout.container.Border',
-    'GeoExt.tree.Panel',
-    'Ext.tree.plugin.TreeViewDragDrop',
-    'GeoExt.panel.Map',
-    'GeoExt.tree.OverlayLayerContainer',
-    'GeoExt.tree.BaseLayerContainer',
-    'GeoExt.data.LayerTreeModel',
-    'GeoExt.tree.View',
-    'GeoExt.tree.Column',
-    'GeoExt.data.MapfishPrintProvider',
-    'GeoExt.panel.PrintMap',
-    'GeoExt.plugins.PrintExtent'
+  'Ext.container.Viewport',
+  'Ext.layout.container.Border',
+  'GeoExt.tree.Panel',
+  'Ext.tree.plugin.TreeViewDragDrop',
+  'GeoExt.panel.Map',
+  'GeoExt.tree.OverlayLayerContainer',
+  'GeoExt.tree.BaseLayerContainer',
+  'GeoExt.data.LayerTreeModel',
+  'GeoExt.tree.View',
+  'GeoExt.tree.Column'
 ]);
 
-
-//var provider2 = Ext.create('GeoExt.data.MapfishPrintProvider', {
-//    method: "GET", //"POST", //recommended for production use
-//    capabilities: printCapabilities, // provide url instead for lazy loading
-//    customParams: {
-//        mapTitle: "当前视图",
-//        comment: ""
-//    }
-//});
-//
-//var printExtent = Ext.create('GeoExt.plugins.PrintExtent', {
-//    printProvider: provider2
-//})
-
-//printExtent.addPage();
 /**
  *  
  */
 Ext.define("core.map.view.MapLayout", {
-    extend: 'Ext.panel.Panel',
-    alias: 'widget.mapLayout',
-    title: "<center height=40>地图信息</center>",
-    closable: true,
-    defaults: {
-        split: true, // 可以设置好看点的折叠效果
-        collapsible: true, // 可以被折叠
-        bodyStyle: 'padding:1px'
-    },
-    layout: 'border',
-    items: [{
-            title: "地区列表",
-            region: 'west',
-            // iconCls:'goodtype_tree',
-            xtype: "orgTreeMap",
-            store: Ext.create('core.map.store.OrgStore'),
-            margins: '5 2 5 5',
-            width: 150
-        }, Ext.create('GeoExt.panel.Map', {
-            alias: 'widget.mapPanel',
-            id: 'mapPanelId',
-            border: true,
-            region: "center",
-            // we do not want all overlays, to try the OverlayLayerContainer
-            map: new OpenLayers.Map({allOverlays: false}),
-            //center: [146.1569825, -41.6109735],
-            //zoom: 6,
-//            plugins: [printExtent],
-            layers: []
-        }),
-        Ext.create('Ext.form.Panel', {
-            id: 'searchPanel',
-            layout: {
-                type: 'hbox',
-                align: 'stretch'
-            },
-            header: false,
-            title: '搜索',
-            region: 'north',
-//            height: 130,
-            width: 300,
-            bodyPadding: 10,
-            defaultType: 'textfield',
-            items: [],
-        })
-
-    ]
+  extend: 'Ext.panel.Panel',
+  alias: 'widget.mapLayout',
+  title: "<center height=40>地图信息</center>",
+  closable: true,
+  defaults: {
+    split: true, // 可以设置好看点的折叠效果
+    collapsible: true, // 可以被折叠
+    bodyStyle: 'padding:1px'
+  },
+  layout: 'border',
+  items: [{
+      title: "地区列表",
+      region: 'west',
+      // iconCls:'goodtype_tree',
+      xtype: "orgTreeMap",
+      store: Ext.create('core.map.store.OrgStore'),
+      margins: '5 2 5 5',
+      width: 150
+    }, Ext.create('GeoExt.panel.Map', {
+      alias: 'widget.mapPanel',
+      id: 'mapPanelId',
+      border: true,
+      region: "center",
+      // we do not want all overlays, to try the OverlayLayerContainer
+      map: new OpenLayers.Map({allOverlays: false}),
+      //center: [146.1569825, -41.6109735],
+      //zoom: 6,
+      layers: [ ]
+    })]
 });
-
